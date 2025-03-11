9a96a753dc862f7419c78c8961499a8dcb6c2f34
WchangQA <154923872+WchangQA@users.noreply.github.com>
2025-03-11 16:07:21 -0700

    Update Queue redirection with wait for element for test environments that run too quickly

--- src/test/java/QueueTests.java
+++ src/test/java/QueueTests.java
@@ -87,7 +87,7 @@ public class QueueTests extends BaseTest {
         albumPage.loadAlbumPage();
         albumPage.selectSongElement();
         QueuePage queuePage = new QueuePage(getThreadLocal());
-        queuePage.soundBarIsDisplayed();
+         queuePage.soundBarIsDisplayed();
         Assert.assertEquals(queuePage.getActualPageUrl(),"https://qa.koel.app/#!/queue");
     }
     @Test
@@ -96,7 +96,7 @@ public class QueueTests extends BaseTest {
         artistPage.loadArtistPage();
         artistPage.selectSongElement();
         QueuePage queuePage = new QueuePage(getThreadLocal());
-        queuePage.soundBarIsDisplayed();
+         queuePage.soundBarIsDisplayed();
         Assert.assertEquals(queuePage.getActualPageUrl(),"https://qa.koel.app/#!/queue");
     }
     @Test
